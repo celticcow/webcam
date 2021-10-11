e431246059519345f9c47a7a3a505b6e36fc4c6a
Gregory Dunlap <gregory.dunlap@gmail.com>
2021-10-10 19:53:31 -0500

    basic code

---
+++ cam1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import cv2
+import sys
+
+def main():
+
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
+    """
+    cascPath = sys.argv[1]
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    video_capture = cv2.VideoCapture(0)
+
+    while(True):
+        #frame by frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+    """
+if __name__ == "__main__":
+    main()
